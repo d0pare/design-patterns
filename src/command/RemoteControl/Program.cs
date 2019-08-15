3e6500779c609d47315c5fbb08e1ac9dcd65146b
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-08-15 18:17:26 -0400

    Change receiver index to an integer constant instead in program.cs.

--- src/command/RemoteControl/Program.cs
+++ src/command/RemoteControl/Program.cs
@@ -6,6 +6,12 @@ namespace RemoteControl
 {
     public class Program
     {
+        const int LIVING_ROOM = 0;
+        const int KITCHEN_LIGHT = 1;
+        const int CEILING_FAN = 2;
+        const int STEREO = 3;
+        const int GARAGE_DOOR = 4;
+
         public static void Main(string[] args)
         {
             var remoteControl = new Invokers.RemoteControl();
@@ -30,21 +36,24 @@ namespace RemoteControl
             StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
             StereoOffCommand stereoOff = new StereoOffCommand(stereo);
 
-            remoteControl.SetCommand(0, livingRoomLightOn, livingRoomLightOff);
-            remoteControl.SetCommand(1, kitchenLightOn, kitchenLightOff);
-            remoteControl.SetCommand(2, ceilingFanOn, ceilingFanOff);
-            remoteControl.SetCommand(3, stereoOnWithCD, stereoOff);
+            remoteControl.SetCommand(LIVING_ROOM, livingRoomLightOn, livingRoomLightOff);
+            remoteControl.SetCommand(KITCHEN_LIGHT, kitchenLightOn, kitchenLightOff);
+            remoteControl.SetCommand(CEILING_FAN, ceilingFanOn, ceilingFanOff);
+            remoteControl.SetCommand(STEREO, stereoOnWithCD, stereoOff);
+            remoteControl.SetCommand(GARAGE_DOOR, garageDoorUp, garageDoorDown);
 
             Console.WriteLine(remoteControl);
 
-            remoteControl.OnButtonWasPushed(0);
-            remoteControl.OffButtonWasPushed(0);
-            remoteControl.OnButtonWasPushed(1);
-            remoteControl.OffButtonWasPushed(1);
-            remoteControl.OnButtonWasPushed(2);
-            remoteControl.OffButtonWasPushed(2);
-            remoteControl.OnButtonWasPushed(3);
-            remoteControl.OffButtonWasPushed(3);
+            remoteControl.OnButtonWasPushed(LIVING_ROOM);
+            remoteControl.OffButtonWasPushed(LIVING_ROOM);
+            remoteControl.OnButtonWasPushed(KITCHEN_LIGHT);
+            remoteControl.OffButtonWasPushed(KITCHEN_LIGHT);
+            remoteControl.OnButtonWasPushed(CEILING_FAN);
+            remoteControl.OffButtonWasPushed(CEILING_FAN);
+            remoteControl.OnButtonWasPushed(STEREO);
+            remoteControl.OffButtonWasPushed(STEREO);
+            remoteControl.OnButtonWasPushed(GARAGE_DOOR);
+            remoteControl.OffButtonWasPushed(GARAGE_DOOR);
 
             Console.ReadKey();
         }
